63185a5fb3657fa95855ff2173c9b6699b1cbc9c
Benjamin Poilve <benjamin.poilve@student.ecp.fr>
2016-12-21 18:42:04 +0100

    Modified conversion routine

--- README.md
+++ README.md
@@ -1,14 +1,33 @@
+
+
+![ ](./static/ressources/logo.png)
 #Lilplay
+
+##Usage
+
+Lilplay is a little electron executable that connects to you google play account and downloads the favorite songs into a choosen folder.
+
+It can remember the informations you gave him and convert to Wav (my phone does not support mp3)
+
+
+![ ](./static/ressources/screen.png)
+
+
+##Story
 
 I love google play music. 
 
 I used to be a music hoarder, obsessed by having full discography of every artists, always comparing with friends how many Gigas of music I had on my drives.. 
 
-When streaming services appeared, I thought it was dumb. Who would pay not to own the music? Seemed weird. 
+When streaming services appeared, I thought it was dumb
+. Who would pay not to own the music? Seemed weird. 
 
 In the end, subscribing to google play was the best things that happened to me! I am always listening different things, sharing playlists with my friends, exploring different style and artists. 
 
 Trouble is, a month ago, my headset claimed another victim. The MDR7506 is probably the greatest headphone I ever owned, but its heavy cable puts too much pressure on the crappy headphones jack that manufacturer use these days. 
+
+![ the silent killer](./static/ressources/killer.jpg)
+
 
 Death toll:
 
@@ -22,6 +41,8 @@ That makes a phone pretty useless.
 
 So once again I was without a proper listening device. Well my replacement dumbphone had a jack, but google play isn't exactly dumbphone friendly!
 
+![a new hope](./static/ressources/hope.png)
+
 I decided to change that! 
 
 ###Disclaimer:
@@ -33,12 +54,10 @@ If your intention is to modify this to make massive downloads on google play, I 
 The fight on music is over, and everybody realised that a good enough service was all that was needed to have people stop pirating. This project goal is to make this service even better, not to advocate breach of TOS. 
 
 
+##FAQ/TroubleShooting
 
-###How it works
+* Talk about [app password](https://support.google.com/accounts/answer/185833?hl=fr) for 2FA users
+* Need to have used google play on the machine before
 
-* Connect your dumbphone as a mass storage
-* Copy the app at root
-* Launch it 
-* Enter your google play email and password
-* Select the music directory, set up a size limit
-* Now each time you want to sync to your last added it will do it automtically! 
+
+
--- static/js/index.js
+++ static/js/index.js
@@ -5,13 +5,6 @@ var fs = require('fs');
 var https = require('https');
 var async = require("async");
 var ffmpeg = require('fluent-ffmpeg');
-
-
-document.querySelector('#p1').addEventListener('mdl-componentupgraded', function() {
-    this.MaterialProgress.setProgress(0);
-});
-
-
 var dataLoaded=false
 var path=""
 var save=true;
@@ -19,8 +12,11 @@ var convert=false;
 var email=""
 var password=""
 var size=16;
+
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    var file = 'data.json'
+    var file = app.getPath(appData)+'/data.json'
     jsonfile.readFile(file, function(err, obj) {
         if(!err){
         document.getElementById("formsync").style.display = "none";
@@ -36,6 +32,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     })
  });
 
+ document.querySelector('#p1').addEventListener('mdl-componentupgraded', function() {
+     this.MaterialProgress.setProgress(0);
+ });
+
+
  var form = document.getElementById('formsync');
  if (form.attachEvent) {
      form.attachEvent("submit", processForm);
@@ -44,53 +45,49 @@ document.addEventListener("DOMContentLoaded", function(event) {
  }
 
  document.getElementById("syncButton").addEventListener("click", function(event) {
-
     if(dataLoaded){
         process();
-
     }else{
     document.getElementById('submitForm').click()
     }
 });
 
 document.getElementById("saved").addEventListener("click", function(event) {
-    fs.unlink('data.json',function(err){
+    fs.unlink(app.getPath(appData)+'/data.json',function(err){
        if(err) return console.log(err);
        location.reload();
   });
-
 });
 
 
-function convertWav(track,callback){
-    ffmpeg(track+".mp3")
+function convertWav(trackin,trackout,callback){
+    ffmpeg(trackin)
     .toFormat('wav')
-.on('error', function (err) {
-    console.log('An error occurred: ' + err.message);
-    callback();
-
-})
-.on('progress', function (progress) {
-    // console.log(JSON.stringify(progress));
-    console.log('Processing: ' + progress.targetSize + ' KB converted');
-})
-.on('end', function () {
-    console.log('Processing finished !');
-    fs.unlink(track+".mp3")
-    callback();
-})
-.save(track+".wav");
+    .on('error', function (err) {
+        console.log('An error occurred: ' + err.message);
+        callback();
+    })
+    .on('progress', function (progress) {
+        console.log('Processing: ' + progress.targetSize + ' KB converted');
+    })
+    .on('end', function () {
+        console.log('Processing finished !');
+        fs.unlink(trackin)
+        callback();
+    })
+    .save(trackout);
 }
 
 function process(){
     pm.init({email: email, password: password}, function(err) {
         if(err){
-            //issue with login
+            document.getElementById("advance").innerHTML="Login error, see <a  target='_blank' href='https://github.com/BenjaminPoilve/LilPlay'>FAQ</a>"
             console.error(err);
             return false
         }
+
         if(save){
-            var file = 'data.json'
+            var file = app.getPath(appData)+'/data.json'
             var obj = {path: path.path,
                        convert: convert,
                        email: email,
@@ -98,14 +95,12 @@ function process(){
                        path: path,
                        size: size,
                     }
-
             jsonfile.writeFile(file, obj, function (err) {
                 console.error(err)
             })
         }
-
-
         pm.getFavorites(function(err, data) {
+            document.getElementById("p1").style.display = "flex";
             usedsize=0;
             var numSong=data.track.length
             index=0;
@@ -114,7 +109,7 @@ function process(){
                 console.log(name);
                 console.log(track.storeId);
                 if (!fs.existsSync(path+"/"+name+".mp3") && !fs.existsSync(path+"/"+name+".wav")) {
-                    var file = fs.createWriteStream(path+"/"+name+".mp3");
+                    var file = fs.createWriteStream("/tmp/"+name+".mp3");
                     pm.getStreamUrl(track.storeId,function(err,streamUrl){
                         console.log(streamUrl)
                         var request = https.get(streamUrl, function(response) {
@@ -123,23 +118,28 @@ function process(){
                                 index+=1;
                                 document.getElementById("advance").innerHTML=index+"/"+numSong
                                 document.querySelector('#p1').MaterialProgress.setProgress(100*index/numSong);
-
-                                usedsize+=getFilesizeInBytes(path+"/"+name+".mp3")
-                                convertWav(path+"/"+name,callback);
+                                if(convert ){
+                                    convertWav("/tmp/"+name+".mp3",path+"/"+name+".wav",callback);
+                                }else{
+                                    fs.createReadStream("/tmp/"+name+".mp3").pipe(fs.createWriteStream(path+"/"+name+".mp3"));
+                                    fs.unlink("/tmp/"+name+".mp3",function(err){
+                                       if(err) return console.log(err);
+                                       callback();
+                                   });
+                                }
                             });
                         });
                     });
                 }else{
                     index+=1;
-
                     document.getElementById("advance").innerHTML=index+"/"+numSong
                     document.querySelector('#p1').MaterialProgress.setProgress(100*index/numSong);
-                    //usedsize+=getFilesizeInBytes(path+"/"+name+".mp3")
-                    convertWav(path+"/"+name,callback);
-                    //callback();
+                    if(convert && fs.existsSync(path+"/"+name+".mp3")){
+                        convertWav(path+"/"+name+".mp3",path+"/"+name+".wav",callback);
+                    }else{
+                        callback();
+                    }
                 }
-
-
             }, function (err) {
                 if (err) { throw err; }
                 console.log(usedsize);
@@ -152,14 +152,11 @@ function process(){
 
 }
 
-
 function getFilesizeInBytes(filename) {
  var stats = fs.statSync(filename)
  var fileSizeInBytes = stats["size"]
  return fileSizeInBytes
 }
-
-
 
 function processForm(e) {
     if (e.preventDefault) e.preventDefault();
